be3f80be630d4499fa6d2298f3dfd03ae829c9bc
Egor Komarov <egor.komarov@bf.rocks>
2023-05-22 11:13:47 +0200

    chore: generate example app

---
+++ example/android/app/src/main/kotlin/com/broxus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.broxus.nekoton_webview_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:nekoton_webview_example/app/app.dart';
+import 'package:nekoton_webview_example/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
