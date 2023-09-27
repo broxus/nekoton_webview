b6a243cfaf0e0bd3999e85c8d7e375f228e12f98
Egor Komarov <Odrin@users.noreply.github.com>
2023-09-27 13:28:16 +0200

    fix: `FullContractState` codegen (#29)
    Co-authored-by: Egor Komarov <egor.komarov@bf.rocks>

--- lib/src/models/full_contract_state.dart
+++ lib/src/models/full_contract_state.dart
@@ -5,9 +5,15 @@ part 'full_contract_state.g.dart';
 
 @JsonSerializable()
 class FullContractState {
-  const FullContractState(this.boc);
+  const FullContractState(this.boc, this.balance, this.genTimings,
+      this.lastTransactionId, this.isDeployed, this.codeHash);
 
   final String boc;
+  final String balance;
+  final GenTimings genTimings;
+  final LastTransactionId? lastTransactionId;
+  final bool isDeployed;
+  final String? codeHash;
 
   factory FullContractState.fromJson(Map<String, dynamic> json) =>
       _$FullContractStateFromJson(json);
