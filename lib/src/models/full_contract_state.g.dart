2b14e9c3fb2b4f44b0e22c943024d49eea98288a
Egor Komarov <egor.komarov@bf.rocks>
2023-09-27 13:11:44 +0200

    fix: `FullContractState` codegen

--- lib/src/models/full_contract_state.g.dart
+++ lib/src/models/full_contract_state.g.dart
@@ -9,9 +9,22 @@ part of 'full_contract_state.dart';
 FullContractState _$FullContractStateFromJson(Map<String, dynamic> json) =>
     FullContractState(
       json['boc'] as String,
+      json['balance'] as String,
+      GenTimings.fromJson(json['genTimings'] as Map<String, dynamic>),
+      json['lastTransactionId'] == null
+          ? null
+          : LastTransactionId.fromJson(
+              json['lastTransactionId'] as Map<String, dynamic>),
+      json['isDeployed'] as bool,
+      json['codeHash'] as String?,
     );
 
 Map<String, dynamic> _$FullContractStateToJson(FullContractState instance) =>
     <String, dynamic>{
       'boc': instance.boc,
+      'balance': instance.balance,
+      'genTimings': instance.genTimings.toJson(),
+      'lastTransactionId': instance.lastTransactionId?.toJson(),
+      'isDeployed': instance.isDeployed,
+      'codeHash': instance.codeHash,
     };
