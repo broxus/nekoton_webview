40cdfc804a6275b4cb3a632ea4c6f9ff1a6dd5de
Egor Komarov <e.komarov.bf@gmail.com>
2025-12-08 13:45:32 +0100

    chore: fix analyze

--- example/lib/app/view/app.dart
+++ example/lib/app/view/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        appBarTheme: const AppBarTheme(),
         colorScheme: ColorScheme.fromSwatch(
           accentColor: const Color(0xFF13B9FF),
         ),
